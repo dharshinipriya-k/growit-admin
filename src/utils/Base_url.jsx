82b83ffe9d2fa700d4e9885582f3fc40444d7aa8
dharshinipriya-k <priyadk1809@gmail.com>
2024-06-13 22:14:42 +0530

    url changed

--- src/utils/Base_url.jsx
+++ src/utils/Base_url.jsx
@@ -1,2 +1,3 @@
+// export const base_url = 'http://localhost:8000/api/'
 
-export const base_url = 'https://backend-growit.onrender.com/api'
+export const base_url = 'https://backend-growit.onrender.com/api/'
